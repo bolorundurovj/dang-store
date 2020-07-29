8672af337a968b91102033ab051f9bd17835fbeb
Bolorunduro Valiant-Joshua <valiantjoshuabolorunduro@gmail.com>
2020-07-29 14:05:34 +0100

    update

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -1,3 +1,7 @@
 exports.homePage = (req, res) => {
     res.render('index', {title: 'Home'});
   }
+
+exports.addStore = (req, res) => {
+    res.render('editStore', {title: '😎😎 Add Store'});
+  }
--- routes/index.js
+++ routes/index.js
@@ -4,5 +4,6 @@ const storeController = require('../controllers/storeController')
 
 // Do work here
 router.get('/', storeController.homePage);
+router.get('/add', storeController.addStore);
 
 module.exports = router;
