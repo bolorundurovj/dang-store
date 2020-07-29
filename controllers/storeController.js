abcf1aee66edc7e63f2166825b93480120133ffd
Bolorunduro Valiant-Joshua <valiantjoshuabolorunduro@gmail.com>
2020-07-29 19:42:35 +0100

    async/await

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -1,3 +1,6 @@
+const mongoose = require('mongoose');
+const Store = mongoose.model('Store');
+
 exports.homePage = (req, res) => {
   res.render("index", { title: "Home" });
 };
@@ -6,7 +9,19 @@ exports.addStore = (req, res) => {
   res.render("editStore", { title: " Add Store" });
 };
 
-exports.createStore = (req, res) => {
-  console.log(req.body);
-  res.json(req.body)
+exports.createStore = async (req, res) => {
+  const store = new Store(req.body);
+  await store.save();
+  // store
+  //   .save()
+  //   .then(store => {
+  //     return Store.find()
+  //   })
+  //   .then(stores => {
+  //     res.render('storeList', {stores: stores})
+  //   })
+  //   .catch(err => {
+  //     throw Error(err)
+  //   })
+  console.log('Store Saved!');
 };
