840a563a52e4f886453d7a3965c85c77cb3f3324
Bolorunduro Valiant-Joshua <valiantjoshuabolorunduro@gmail.com>
2020-07-30 13:35:03 +0100

    Edit Stores

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -1,5 +1,5 @@
-const mongoose = require('mongoose');
-const Store = mongoose.model('Store');
+const mongoose = require("mongoose");
+const Store = mongoose.model("Store");
 
 exports.homePage = (req, res) => {
   res.render("index", { title: "Home" });
@@ -10,9 +10,12 @@ exports.addStore = (req, res) => {
 };
 
 exports.createStore = async (req, res) => {
-  const store = await (new Store(req.body)).save();
-  req.flash('success', `Successfully created ${store.name}. Care to leave a review?`);
-  res.redirect(`/store/${store.slug}`);
+  const store = await new Store(req.body).save();
+  req.flash(
+    "success",
+    `Successfully created ${store.name}. Care to leave a review?`
+  );
+  res.redirect(`/stores/${store.slug}`);
   // store
   //   .save()
   //   .then(store => {
@@ -28,5 +31,22 @@ exports.createStore = async (req, res) => {
 
 exports.getStores = async (req, res) => {
   const stores = await Store.find();
-  res.render('stores', {title: "Stores", stores})
-}
+  res.render("stores", { title: "Stores", stores });
+};
+
+exports.editStore = async (req, res) => {
+  const store = await Store.findOne({ _id: req.params.id });
+  res.render("editStore", { title: `Edit ${store.name}`, store });
+};
+
+exports.updateStore = async (req, res) => {
+  const store = await Store.findOneAndUpdate({ _id: req.params.id }, req.body, {
+    new: true,
+    runValidators: true,
+  }).exec();
+  req.flash(
+    "success",
+    `Successfully updated <strong>${store.name}</strong>. <a href="/stores/${store.slug}">View Store &rarr; </a>`
+  );
+  res.redirect(`/stores/${store._id}/edit`);
+};
