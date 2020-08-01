2daf75ba8bd4218bb4cfe4d4a4fd9ad86fae997d
Bolorunduro Valiant-Joshua <valiantjoshuabolorunduro@gmail.com>
2020-08-01 16:17:59 +0100

    forgot password email

--- controllers/authController.js
+++ controllers/authController.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose");
 const User = mongoose.model("User");
 const crypto = require("crypto");
 const promisify = require("es6-promisify");
+const mail = require('../handlers/mail');
 
 exports.login = passport.authenticate("local", {
   failureRedirect: "/login",
@@ -39,9 +40,17 @@ exports.forgot = async (req, res) => {
   await user.save();
 
   const resetURL = `http://${req.headers.host}/account/reset/${user.resetPasswordToken}`;
+
+  await mail.send({
+    user,
+    subject: 'Password Reset',
+    resetURL,
+    filename: 'password-reset'
+  });
+
   req.flash(
     "success",
-    `You have been emailed a password reset link. ${resetURL}`
+    `You have been emailed a password reset link.`
   );
 
   res.redirect("/login");
---
+++ handlers/mail.js
@@ -0,0 +1,45 @@
+const nodemailer = require("nodemailer");
+const pug = require("pug");
+const juice = require("juice");
+const htmlToText = require("html-to-text");
+const promisify = require("es6-promisify");
+
+const transport = nodemailer.createTransport({
+  host: process.env.MAIL_HOST,
+  port: process.env.MAIL_PORT,
+  auth: {
+    user: process.env.MAIL_USER,
+    pass: process.env.MAIL_PASS,
+  },
+});
+
+const generateHTML = (filename, options = {}) => {
+  const html = pug.renderFile(
+    `${__dirname}/../views/email/${filename}.pug`,
+    options
+  );
+  const inlined = juice(html)
+  return inlined;
+};
+
+exports.send = async (options) => {
+  const html = generateHTML(options.filename, options);
+  const text = htmlToText.fromString(html);
+  const mailOptions = {
+    from: `Vee Jay <vee-jay@vj.com>`,
+    to: options.user.email,
+    subject: options.subject,
+    html,
+    text,
+  };
+  const sendMail = promisify(transport.sendMail, transport);
+  return sendMail(mailOptions);
+};
+
+// transport.sendMail({
+//   from: "Vee Jay <vee-jay@vj.com>",
+//   to: "randy@southpark.com",
+//   subject: "I am lorde!",
+//   html: "I am <strong>lorde</strong> ya ya ya",
+//   text: "I am **lorde** ya ya ya",
+// });
